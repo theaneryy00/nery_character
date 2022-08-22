6e337a79a3a8e4cff8f3ffbc97965075f16a4b5b
theaneryy00 <111473139+theaneryy00@users.noreply.github.com>
2022-08-22 11:14:39 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,53 @@
+public class App {
+    public static void main(String[] args) {
+
+        Character knight = new Character();
+
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "Chen";
+        knight.sayMyName();
+        knight.sayMyStrength();
+        knight.sayMyAgility();
+        knight.sayMyIntellegence();
+
+
+        Character mage = new Character();
+
+        mage.strength = 0;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Akasha";
+        mage.sayMyName();
+        mage.sayMyStrength();
+        mage.sayMyAgility();
+        mage.sayMyIntellegence();
+
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        thief.name = "Gondar";
+        thief.sayMyName();
+        thief.sayMyStrength();
+        thief.sayMyAgility();
+        thief.sayMyIntellegence();
+    
+
+
+        Character archer = new Character();
+
+        archer.strength = 5;
+        archer.agility = 5;
+        archer.intelligence = 5;
+        archer.name = "Aeri";
+        archer.sayMyName();
+        archer.sayMyStrength();
+        archer.sayMyAgility();
+        archer.sayMyIntellegence();
+    
+        
+    } 
+}
---
+++ Character.java
@@ -0,0 +1,22 @@
+public class Character {
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("My name is " + name + "\n");
+    }
+    public void sayMyStrength() {
+        System.out.println("My strength is " + strength );
+     }
+    public void sayMyAgility() {
+        System.out.println("My Agility is is " + agility);
+    }
+    public void sayMyIntellegence() {
+        System.out.println("My Intelligence is " + name);
+     }
+
+}
+    
+
